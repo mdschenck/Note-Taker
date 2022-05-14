83230368aaecf6d8a68e3012c1cc41b5f8b9c009
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-05-14 18:42:02 -0400

    fixed PORT issue hopefully

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
